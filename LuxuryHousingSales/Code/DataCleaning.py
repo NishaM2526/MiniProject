8698d866a09eeb8c85f1bba237133ed89268095d
Nisha <nisha.ahsin90@gmail.com>
2026-04-01 12:09:37 +0530

    miniproject2

---
+++ LuxuryHousingSales/Code/DataCleaning.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import os
+
+base_dir = os.path.dirname(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir))
+file_path = os.path.join(base_dir, "Data", "Luxury_Housing_Bangalore.csv")
+df = pd.read_csv(file_path)
+
+def data_preprocessing():
+    # Creating a copy of the original DataFrame to work with
+    df_copy = df.copy()
+
+    # Clean inconsistent formats
+    df_copy['Ticket_Price_Cr']=df_copy['Ticket_Price_Cr'].str.replace('Cr','')
+    df_copy['Ticket_Price_Cr']=df_copy['Ticket_Price_Cr'].str.replace('₹','')
+    df_copy['Ticket_Price_Cr']=df_copy['Ticket_Price_Cr'].astype(float)
+
+    # Normalize text fields
+    df_copy['Configuration']=df_copy['Configuration'].str.upper()
+    df_copy['Micro_Market'] = df_copy['Micro_Market'].str.strip().str.title()
+    df_copy['NRI_Buyer'] = df_copy['NRI_Buyer'].str.strip().str.title()
+
+    # Handle missing values
+    df_copy=df_copy.dropna(subset=['Unit_Size_Sqft','Ticket_Price_Cr','Amenity_Score'], how='all') 
+    df_copy=df_copy.dropna(subset=['Unit_Size_Sqft','Ticket_Price_Cr'], how='all') 
+    df_copy=df_copy.fillna(
+                            {   
+                                'Unit_Size_Sqft':df_copy['Unit_Size_Sqft'].mean(),
+                                'Ticket_Price_Cr':df_copy['Ticket_Price_Cr'].median(),
+                                'Amenity_Score':df_copy['Amenity_Score'].median()
+                            }
+                        )
+    df_copy=df_copy.fillna({'Buyer_Comments': df_copy['Buyer_Comments'].ffill()})
+
+    # Handle duplicates
+    df_copy=df_copy.drop_duplicates(keep='first')
+
+    # Derive columns
+    df_copy['Price_per_Sqft'] = ((df_copy['Ticket_Price_Cr']*10000000)/df_copy['Unit_Size_Sqft']).round(2)
+    df_copy['Purchase_Quarter']=pd.to_datetime(df_copy['Purchase_Quarter'])
+    df_copy['Quarter_Number']=df_copy['Purchase_Quarter'].dt.quarter
+    df_copy['Booking_Flag']=df_copy['Transaction_Type'].apply(lambda x: 1 if x=='Primary' else 0)
+
+    return df_copy
+
+#df_data_cleaned = data_preprocessing()
+#print(df_data_cleaned.head())
---
+++ LuxuryHousingSales/Code/LoadData.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import mysql.connector
+import DataCleaning as dc
+
+database_connection = mysql.connector.connect(
+    user="root",
+    host="localhost",
+    password="Nisha@2506",
+    database="luxury_housing_sales"
+)
+my_cursor = database_connection.cursor()
+# my_cursor.execute("create database luxury_housing_sales")
+
+def sql_statements():
+    my_cursor.execute("""create table if not exists luxury_housing_bglr(
+                        Property_ID           varchar(25),        
+                        Micro_Market          varchar(25),
+                        Project_Name          varchar(25),
+                        Developer_Name        varchar(25),
+                        Unit_Size_Sqft        float,
+                        Configuration         varchar(25),
+                        Ticket_Price_Cr       float,      
+                        Transaction_Type      varchar(25),        
+                        Buyer_Type            varchar(25),        
+                        Purchase_Quarter      date,
+                        Connectivity_Score    float,       
+                        Amenity_Score         float,       
+                        Possession_Status     varchar(25),        
+                        Sales_Channel         varchar(25),        
+                        NRI_Buyer             varchar(10),        
+                        Locality_Infra_Score  float,       
+                        Avg_Traffic_Time_Min  int,         
+                        Buyer_Comments        varchar(255),        
+                        Price_per_Sqft        float,        
+                        Quarter_Number        int,          
+                        Booking_Flag          int
+                      )""")
+    # insert into database
+    df_data = dc.data_preprocessing().values.tolist()
+    query ="insert into luxury_housing_bglr values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+    for i in df_data:
+        my_cursor.execute(query, tuple(i))
+    database_connection.commit()
+
+sql_statements()
+my_cursor.close()
+database_connection.close()
